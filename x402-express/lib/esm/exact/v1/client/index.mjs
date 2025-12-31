dda7d29b1e0a1b9672fb5e97b5ade2b805c8d296
Vivek Sahu <viveksahu1762@gmail.com>
2026-01-01 00:32:50 +0530

    chore: update package.json and remove deprecated files
    - Added dependencies for x402-evm-mantle and x402-svm-mantle.
    - Removed obsolete TypeScript declaration files and JavaScript files to clean up the project structure.
    - Streamlined the codebase by eliminating unused modules and improving overall maintainability.

--- x402-express/lib/esm/exact/v1/client/index.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  ExactSvmSchemeV1
-} from "../../../chunk-EEA7DKZI.mjs";
-import "../../../chunk-IKSTWKEM.mjs";
-export {
-  ExactSvmSchemeV1
-};
-//# sourceMappingURL=index.mjs.map
--- x402-express/lib/esm/exact/v1/facilitator/index.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  ExactSvmSchemeV1
-} from "../../../chunk-3CEIVWNN.mjs";
-import "../../../chunk-IKSTWKEM.mjs";
-export {
-  ExactSvmSchemeV1
-};
-//# sourceMappingURL=index.mjs.map
